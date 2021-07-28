684b17c47e7f0969b500dd943585b829f2810bef
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-28 23:56:18 +0300

    added todos and users in state

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,36 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    todos: [],
+    users: [],
   },
   mutations: {
+    setTodos(state, todos) {
+      state.todos = todos;
+    },
+    setUsers(state, users) {
+      state.users = users;
+    },
   },
   actions: {
+    fetchTodos({ commit }) {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => response.json())
+        .then((todos) => commit('setTodos', todos));
+    },
+    fetchUsers({ commit }) {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((users) => commit('setUsers', users));
+    },
   },
-  modules: {
+  getters: {
+    tableItems(state) {
+      return state.todos.map((todo) => ({
+        ...todo,
+        completed: todo.completed ? 'Done' : 'In Progress',
+        user: state.users.length ? state.users.find((user) => user.id === todo.userId).name : '',
+      }));
+    },
   },
 });

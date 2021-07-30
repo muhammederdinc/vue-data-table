fdedc4b7adab7f81d999179adb343d0f78cdce66
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-30 14:23:50 +0300

    added dialog.scss

--- src/components/VDialog.vue
+++ src/components/VDialog.vue
@@ -42,38 +42,40 @@ export default {
 </script>
 
 <template>
-  <div class="modal-mask">
-    <div class="modal-wrapper">
-      <div class="modal-container">
-        <div class="modal-header">
+  <div class="v-dialog">
+    <div class="v-dialog-wrapper">
+      <div class="v-dialog-container">
+        <div class="v-dialog-header">
           <slot name="header">
-            Edit Item
+            Edit Todo
           </slot>
         </div>
 
-        <div class="modal-body">
+        <div class="v-dialog-body">
           <slot name="body">
-            <input v-model="formData.title" class="input" type="text">
+            <div class="v-dialog-form">
+              <input v-model="formData.title" class="input" type="text">
 
-            <br />
+              <br />
 
-            <input v-model="formData.completed" type="checkbox">
-            <label>Completed</label>
+              <input v-model="formData.completed" type="checkbox">
+              <label>Completed</label>
+            </div>
           </slot>
         </div>
 
-        <div class="modal-footer">
+        <div class="v-dialog-footer">
           <slot name="footer">
             <button
-              v-text="'Save'"
-              class="btn btn--primary modal-default-button"
-              @click="submit"
+              v-text="'Close'"
+              class="btn btn--error"
+              @click="$emit('close')"
             />
 
             <button
-              v-text="'Close'"
-              class="btn btn--error modal-default-button"
-              @click="$emit('close')"
+              v-text="'Save'"
+              class="btn btn--primary"
+              @click="submit"
             />
           </slot>
         </div>
@@ -81,63 +83,3 @@ export default {
     </div>
   </div>
 </template>
-
-<style>
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
-}
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
-  width: 400px;
-  margin: 0px auto;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-}
-
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-footer {
-  padding-bottom: 26px;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-.modal-enter {
-  opacity: 0;
-}
-
-.modal-leave-active {
-  opacity: 0;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
-</style>
